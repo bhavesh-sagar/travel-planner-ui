7919250ec0caf39c29cd514d9799811321cad7f9
bhavesh-sagar <bhaveshnt9@gmail.com>
2026-04-26 20:42:13 +0530

    fixing routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ResultsComponent } from './components/results/results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'results', component: ResultsComponent },
+  { path: '**', redirectTo: '' }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
